536e969b4ffe4d682cf4500582ee86ebe284d99d
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-10-31 01:19:59 -0600

    remove unused css files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>MineGiver</title>
-        <meta name="description" content="minecraft /give command generator" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div>
+			<Head>
+				<title>Minecraft Command Generator</title>
+				<meta
+					name="description"
+					content="minecraft command generator"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          MineGiver
-        </h1>
-      </main>
-    </div>
-  )
+			<main>
+				<h1>MineGiver</h1>
+			</main>
+		</div>
+	);
 }
