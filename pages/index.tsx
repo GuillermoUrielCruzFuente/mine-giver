905cffdc9125eb8c9fd3179cb0163c5b0348637f
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-01 01:24:38 -0600

    feature list added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,40 @@
 import Head from "next/head";
+import styles from "../src/styles/pages/Home.module.scss";
+
+import Feature, { FeatureType } from "../src/components/Feature";
+
+const features: Array<FeatureType> = [
+	{
+		name: "give",
+	},
+	{
+		name: "enchant",
+	},
+	{
+		name: "state",
+	},
+	{
+		name: "locate",
+	},
+	{
+		name: "difficulty",
+	},
+	{
+		name: "experience",
+	},
+	{
+		name: "gamerule",
+	},
+	{
+		name: "loot",
+	},
+];
 
 export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>Minecraft Command Generator</title>
+				<title>MineGiver | Minecraft Command Generator</title>
 				<meta
 					name="description"
 					content="minecraft command generator"
@@ -12,8 +42,17 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main>
-				<h1>MineGiver</h1>
+			<main className={styles.mainContainer}>
+				<h1 className={styles.title}>MineGiver</h1>
+				<p className={styles.text}>
+					An easy eay to create powerful minecraft commands.
+				</p>
+
+				<div className={styles.featuresContainer}>
+					{features.map((feat: FeatureType) => {
+						return <Feature key={feat.name} name={feat.name} />;
+					})}
+				</div>
 			</main>
 		</div>
 	);
