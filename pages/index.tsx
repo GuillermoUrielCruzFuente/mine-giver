05b1dd105fc3bd8e74fbe97145e97d15eb14f1a2
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-02 18:50:36 -0600

    update features

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+//next imports
 import Head from "next/head";
+import Link from "next/link";
+
+//styles
 import styles from "../src/styles/pages/Home.module.scss";
 
+//custom components
 import Feature, { FeatureType } from "../src/components/Feature";
 
 const features: Array<FeatureType> = [
@@ -34,25 +39,33 @@ export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>MineGiver | Minecraft Command Generator</title>
+				<title>MineGiver | Minecraft Utils</title>
 				<meta
 					name="description"
-					content="minecraft command generator"
+					content="MineGiver is a webapp that helps players to create and search some related things about the game them love"
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<main className={styles.mainContainer}>
 				<h1 className={styles.title}>MineGiver</h1>
-				<p className={styles.text}>
-					An easy eay to create powerful minecraft commands.
-				</p>
+				<p className={styles.text}>A tool to help players</p>
 
+				<hr />
+
+				<h2>Command generator</h2>
 				<div className={styles.featuresContainer}>
 					{features.map((feat: FeatureType) => {
 						return <Feature key={feat.name} name={feat.name} />;
 					})}
 				</div>
+
+				<hr />
+
+				<h2>Player related stuff</h2>
+				<Link href="/players">Search player info</Link>
+
+				<hr />
 			</main>
 		</div>
 	);
