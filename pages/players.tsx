03af8f3252d0f6652061cbbd9eeb869c2178b048
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-06 14:28:31 -0600

    code modularization and better imports

--- pages/players.tsx
+++ pages/players.tsx
@@ -1,57 +1,12 @@
-import Head from "next/head";
-import { SyntheticEvent, useEffect, useRef, useState } from "react";
-import styles from "../src/styles/pages/Players.module.scss";
 import Image from "next/image";
-//images
-import compassGif from "../src/static/img/compass.webp";
-import steve from "../src/static/img/steve-using-spyglass.webp";
-
-type Player = {
-	avatar: string;
-	id: string;
-	meta: Object;
-	name_history: Array<string>;
-	raw_id: string;
-	username: string;
-};
+import Head from "next/head";
+import { useState } from "react";
+import styles from "@/styles/pages/Players.module.scss";
+import PlayerSearchBar from "@/components/PlayerSearchBar";
+import { Player } from "@/utils/fetchPlayer";
 
 const Players = () => {
-	const searchBarRef = useRef<HTMLInputElement>(null);
-	const [playerData, setPlayerData] = useState<Player | null>();
-
-	const fetchPlayerData = async (player: string) => {
-		const PLAYER_ENDPOINT = `https://playerdb.co/api/player/minecraft/${player}`;
-
-		try {
-			const playerDataRequest = await fetch(PLAYER_ENDPOINT);
-
-			if (playerDataRequest.status === 400) {
-				noPlayerFound();
-				throw new Error("The player name does not exist");
-			}
-
-			const playerData = await playerDataRequest.json();
-
-			setPlayerData(playerData.data.player as Player);
-		} catch (error) {
-			console.error(error);
-		}
-	};
-
-	const handleSearch = (event: SyntheticEvent) => {
-		event.preventDefault();
-
-		if (searchBarRef.current) {
-			const searchTerm = searchBarRef.current.value;
-			searchTerm === ""
-				? fetchPlayerData("Melatoninan")
-				: fetchPlayerData(searchTerm);
-		}
-	};
-
-	const noPlayerFound = () => {
-		setPlayerData(null);
-	};
+	const [playerData, setPlayerData] = useState<Player | undefined>();
 
 	return (
 		<div>
@@ -71,34 +26,7 @@ const Players = () => {
 					in the box below.
 				</p>
 
-				<form className={styles.searchForm} onSubmit={handleSearch}>
-					<input
-						ref={searchBarRef}
-						type="text"
-						placeholder="username/uuid"
-					/>
-					<div className={styles.buttonContainer}>
-						<button type="submit">
-							search
-							{/* the parent div below prevents img overflow its parent (button) */}
-							<div>
-								<Image
-									className={styles.compass}
-									src={compassGif}
-									alt="minecraft compass"
-								/>
-							</div>
-						</button>
-
-						<Image
-							className={styles.steveImg}
-							src={steve}
-							alt="steve using the spyglass"
-						/>
-
-						<div className={styles.playerImgPatch}></div>
-					</div>
-				</form>
+				<PlayerSearchBar dataSetter={setPlayerData} />
 
 				{!!playerData ? (
 					<div className={styles.playerInfoContainer}>
