ea4b24965ae0cc2739f7b2f41937e47a54bef95b
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-06 14:24:56 -0600

    move fetchPlayer func to its own file

---
+++ src/modules/utils/fetchPlayer.ts
@@ -0,0 +1,30 @@
+export type Player = {
+	avatar: string;
+	id: string;
+	raw_id: string;
+	username: string;
+};
+
+const PLAYER_ENDPOINT = "https://playerdb.co/api/player/minecraft/";
+
+const playerNotFound = (response: Response) =>
+	response.status > 400 && response.status < 500 && !response.ok;
+
+const fetchPlayer = async (identifier: string): Promise<Player | undefined> => {
+	try {
+		const playerRequest = await fetch(PLAYER_ENDPOINT + identifier);
+
+		if (playerNotFound(playerRequest)) {
+			throw new Error("That player does not exist");
+		}
+
+		const playerData = await playerRequest.json();
+
+		return playerData.data.player as Player;
+	} catch (error) {
+		console.log(error);
+		return undefined;
+	}
+};
+
+export default fetchPlayer;
