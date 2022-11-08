c795997a7f8620ec7ab865d906c5242cd39a8708
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-07 23:20:24 -0600

    enter/exit animation

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,20 +1,28 @@
-import { motion } from "framer-motion";
+import { Player } from "@/utils/fetchPlayer";
+import { motion, AnimatePresence } from "framer-motion";
 
-const PlayerCard = () => {
+const PlayerCard = ({ data }: { data?: Player }) => {
 	return (
-		<motion.div
-			initial={{ opacity: 0, translateY: 30 }}
-			animate={{ opacity: 1, translateY: 0 }}
-			transition={{ duration: 0.5 }}
-			style={{ color: "#333" }}
-		>
-			<h1>Hola</h1>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-				Eveniet illo earum molestiae vero quos, rem magnam. Vitae
-				distinctio tenetur repellat?
-			</p>
-		</motion.div>
+		<AnimatePresence>
+			{!!data && (
+				<motion.div
+					initial={{ opacity: 0, translateY: 35 }}
+					animate={{ opacity: 1, translateY: 0 }}
+					exit={{ opacity: 0, rotate: 90, scale: 0.85 }}
+					transition={{ duration: 0.35 }}
+					style={{
+						color: "#333",
+						border: "dashed 2px #fff",
+						padding: "1rem",
+						borderRadius: 16,
+						marginTop: "3rem",
+					}}
+				>
+					<h1>{data.username}</h1>
+					<h2>{data.id}</h2>
+				</motion.div>
+			)}
+		</AnimatePresence>
 	);
 };
 
