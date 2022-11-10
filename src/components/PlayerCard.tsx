5deae85d89c8c1e7633cace871d4df6754c09e8d
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-09 19:05:38 -0600

    add more imagery

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -14,7 +14,43 @@ const PlayerCard = ({ data }: { data?: Player }) => {
 					className={styles.playerContainer}
 				>
 					<h1>{data.username}</h1>
-					<h2>{data.id}</h2>
+
+					<div className={styles.info}>
+						<p className={styles.tag}>raw id</p>
+						<p>{data.raw_id}</p>
+					</div>
+
+					<div className={styles.info}>
+						<p className={styles.tag}>id</p>
+						<p>{data.id}</p>
+					</div>
+
+					<h2>skins</h2>
+					<div className={styles.imgContainer}>
+						<img
+							src={data.avatar}
+							alt={`${data.username}'s head, frontal render`}
+							title={`${data.username}'s head, frontal render`}
+						/>
+
+						<img
+							src={`https://crafthead.net/cube/${data.raw_id}`}
+							alt={`${data.username}'s head, isometric render`}
+							title={`${data.username}'s head, isometric render`}
+						/>
+
+						<img
+							src={`https://crafthead.net/body/${data.raw_id}`}
+							alt={`${data.username}'s full body, frontal render`}
+							title={`${data.username}'s full body, frontal render`}
+						/>
+
+						<img
+							src={`https://crafthead.net/skin/${data.raw_id}`}
+							alt={`${data.username}'s skin`}
+							title={`${data.username}'s skin`}
+						/>
+					</div>
 				</motion.div>
 			)}
 		</AnimatePresence>
