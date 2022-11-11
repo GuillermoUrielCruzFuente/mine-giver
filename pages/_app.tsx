cafe5390de9a64e3f15f08dbb86686e8371ef69c
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-10 21:59:20 -0600

    add nav to layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../src/styles/globals.scss";
 import type { AppProps } from "next/app";
+import Nav from "@/components/Nav";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Nav />
+			<Component {...pageProps} />
+		</>
+	);
 }
