3c228e6b1a16a27c9b3a262e5b41406271ac4bbe
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-13 14:43:55 -0600

    add an id visualizer component

--- src/components/IDVisualizer.tsx
+++ src/components/IDVisualizer.tsx
@@ -1,13 +1,67 @@
 import { Player } from "@/utils/fetchPlayer";
+import styles from "@/styles/components/IDVisualizer.module.scss";
+import { useEffect, useState } from "react";
 
 type Identifiers = Pick<Player, "id" | "rawId">;
 
 const IDVisualizer = ({ id, rawId }: Identifiers) => {
+	const [selection, setSelection] = useState(rawId);
+
+	const copyToClipBoard = () => {
+		navigator.clipboard.writeText(selection);
+	};
+
+	const setInputValueToId = () => {
+		setSelection(id);
+	};
+
+	const setInputValueToRawId = () => {
+		setSelection(rawId);
+	};
+
+	useEffect(() => {
+		setInputValueToRawId();
+	}, [id, rawId]);
+
 	return (
-		<>
-			<p>{id}</p>
-			<p>{rawId}</p>
-		</>
+		<div className={styles.IDVisualizerContainer}>
+			<p className={styles.topic}>UUID</p>
+
+			<div className={styles.switcher}>
+				<div className={styles.switcherButtonsContainer}>
+					<button
+						className={
+							selection === rawId
+								? styles.switcherButtonActive
+								: styles.switcherButton
+						}
+						onClick={setInputValueToRawId}
+					>
+						raw
+					</button>
+					<button
+						className={
+							selection === id
+								? styles.switcherButtonActive
+								: styles.switcherButton
+						}
+						onClick={setInputValueToId}
+					>
+						full
+					</button>
+				</div>
+
+				<div className={styles.idCoppier}>
+					<input
+						className={styles.idInput}
+						type="text"
+						value={selection}
+						readOnly
+					/>
+					<button onClick={copyToClipBoard}>copy</button>
+				</div>
+			</div>
+		</div>
 	);
 };
 
