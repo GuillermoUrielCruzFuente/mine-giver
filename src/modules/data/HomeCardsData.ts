4820164c65fe9e3091ef753119c35524402d1b71
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-24 21:51:55 -0600

    rename file

--- src/modules/data/HomeCardsData.ts
+++ src/modules/data/HomeCardsData.ts
@@ -2,6 +2,10 @@ export type HomeCardProps = {
 	title: string;
 	description: string;
 	route: string;
+	imageBGColor: string;
+	titleColor: string;
+	descriptionColor: string;
+	buttonColor: string;
 };
 
 const HomeCardsData: Array<HomeCardProps> = [
@@ -10,24 +14,40 @@ const HomeCardsData: Array<HomeCardProps> = [
 		description:
 			"Create your own commands, store and share them with your friends.",
 		route: "/commands",
+		imageBGColor: "",
+		titleColor: "",
+		descriptionColor: "",
+		buttonColor: "",
 	},
 	{
 		title: "players",
 		description:
 			"Download skins, search your favorite Player and explore in a great 3D interface.",
 		route: "/players",
+		imageBGColor: "",
+		titleColor: "",
+		descriptionColor: "",
+		buttonColor: "",
 	},
 	{
 		title: "technical mods",
 		description:
 			"Stay updated with the most popular Technical mod packs and never lost a release.",
 		route: "/mods",
+		imageBGColor: "",
+		titleColor: "",
+		descriptionColor: "",
+		buttonColor: "",
 	},
 	{
 		title: "blog",
 		description:
 			"Read the last news in the minecraft community, some tips and opinions.",
 		route: "/blog",
+		imageBGColor: "",
+		titleColor: "",
+		descriptionColor: "",
+		buttonColor: "",
 	},
 ];
 
