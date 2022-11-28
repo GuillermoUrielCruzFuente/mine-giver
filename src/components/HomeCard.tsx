eee8a6a672ebc1c5312fceaf51cb535e65f1f29d
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-27 19:55:11 -0600

    add custom colors to card

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,17 +1,24 @@
-import { HomeCardProps } from "@/data/HomeCards";
+import { HomeCardProps } from "@/data/HomeCardsData";
 import Link from "next/link";
 import styles from "@/styles/components/HomeCard.module.scss";
 
-const HomeCard = ({ title, description, route }: HomeCardProps) => {
+const HomeCard = ({ title, description, route, colors }: HomeCardProps) => {
 	return (
 		<article className={styles.homeCard}>
-			<div className={styles.imgContainer}>
-			</div>
+			<div
+				className={styles.imgContainer}
+				style={{ backgroundColor: colors.imageBG }}
+			></div>
 
-			<div className={styles.infoContainer}>
-				<h1>{title}</h1>
-				<p>{description}</p>
-				<Link href={route}>{route}</Link>
+			<div
+				className={styles.infoContainer}
+				style={{ backgroundColor: colors.infoBG }}
+			>
+				<h1 style={{ color: colors.title }}>{title}</h1>
+				<p style={{ color: colors.text }}>{description}</p>
+				<Link href={route} style={{ backgroundColor: colors.button }}>
+					{route}
+				</Link>
 			</div>
 		</article>
 	);
